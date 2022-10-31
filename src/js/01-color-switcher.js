6f1914aecdd4c0660593f7ca975e012273477fa3
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-31 18:42:07 +0200

    task#1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  launchBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  documentBody: document.body,
+};
 
+let timerId = null;
+
+toggleBtnAvailability(refs.launchBtn, refs.stopBtn);
+
+refs.launchBtn.addEventListener('click', startChangeBackgroundColor);
+refs.stopBtn.addEventListener('click', stopChangeBackgroundColor);
+
+function startChangeBackgroundColor() {
+  toggleBtnAvailability(refs.stopBtn, refs.launchBtn);
+  setBodyBackgroundColor(refs.documentBody);
+
+  timerId = setInterval(setBodyBackgroundColor, 1000, refs.documentBody);
+}
+
+function stopChangeBackgroundColor() {
+  clearInterval(timerId);
+  toggleBtnAvailability(refs.launchBtn, refs.stopBtn);
+}
+
+function toggleBtnAvailability(enabledBtn, disabledBtn) {
+  enabledBtn.removeAttribute('disabled', 'disabled');
+  disabledBtn.setAttribute('disabled', 'disabled');
+}
+
+function setBodyBackgroundColor(element) {
+  element.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
