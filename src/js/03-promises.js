38a2689782dd4ae9ad3c4ab4f2e0259ffdfb59f6
Oleksandr Romanchuk <novator0511@gmail.com>
2022-11-02 15:09:43 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,12 +6,20 @@ const {
   task03: { form },
 } = refs;
 
+[...form.elements].forEach(({ name }) => {
+  if (name) {
+    name === 'amount'
+      ? form.elements[name].setAttribute('min', 1)
+      : form.elements[name].setAttribute('min', 0);
+  }
+});
+
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  createDataObj(form);
+  let { delay, step, amount } = createDataObj(form);
 
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay)
@@ -24,13 +32,13 @@ function onFormSubmit(event) {
 }
 
 function createDataObj(formEl) {
-  return ({ delay, step, amount } = [...formEl].reduce((dataObj, { name }) => {
+  return [...formEl].reduce((dataObj, { name }) => {
     if (name) {
       dataObj[name] = Number(formEl.elements[name].value);
     }
 
     return dataObj;
-  }, {}));
+  }, {});
 }
 
 function ifPromiseResolve({ position, delay }) {
